d8882f7d96be6ba615cb573ee634e699a9baf642
Julian Scheffers <julian@scheffers.net>
2025-08-28 17:24:51 +0200

    Add more symbols from POSIX and BadgeELF API

--- src/kbelf_lib.c
+++ src/kbelf_lib.c
@@ -3,6 +3,8 @@
 
 #include <kbelf.h>
 
+extern char const symbol_asp_device_get_name[] asm("asp_device_get_name");
+extern char const symbol_asp_device_get_manufacturer[] asm("asp_device_get_manufacturer");
 extern char const symbol_asp_disp_get_params[] asm("asp_disp_get_params");
 extern char const symbol_asp_disp_get_fb[] asm("asp_disp_get_fb");
 extern char const symbol_asp_disp_get_pax_buf[] asm("asp_disp_get_pax_buf");
@@ -13,6 +15,63 @@ extern char const symbol_asp_disp_write_part[] asm("asp_disp_write_part");
 extern char const symbol_asp_disp_write_part_pax[] asm("asp_disp_write_part_pax");
 extern char const symbol_asp_err_id[] asm("asp_err_id");
 extern char const symbol_asp_err_desc[] asm("asp_err_desc");
+extern char const symbol_asp_input_poll[] asm("asp_input_poll");
+extern char const symbol_asp_input_get_nav[] asm("asp_input_get_nav");
+extern char const symbol_asp_input_get_action[] asm("asp_input_get_action");
+extern char const symbol_asp_input_needs_on_screen_keyboard[] asm("asp_input_needs_on_screen_keyboard");
+extern char const symbol_asp_input_set_backlight[] asm("asp_input_set_backlight");
+extern char const symbol_asp_input_get_backlight[] asm("asp_input_get_backlight");
+extern char const symbol_sched_yield[] asm("sched_yield");
+extern char const symbol_sched_get_priority_min[] asm("sched_get_priority_min");
+extern char const symbol_sched_get_priority_max[] asm("sched_get_priority_max");
+extern char const symbol_pthread_mutexattr_init[] asm("pthread_mutexattr_init");
+extern char const symbol_pthread_mutexattr_destroy[] asm("pthread_mutexattr_destroy");
+extern char const symbol_pthread_mutexattr_gettype[] asm("pthread_mutexattr_gettype");
+extern char const symbol_pthread_mutexattr_settype[] asm("pthread_mutexattr_settype");
+extern char const symbol_pthread_mutex_init[] asm("pthread_mutex_init");
+extern char const symbol_pthread_mutex_destroy[] asm("pthread_mutex_destroy");
+extern char const symbol_pthread_mutex_lock[] asm("pthread_mutex_lock");
+extern char const symbol_pthread_mutex_trylock[] asm("pthread_mutex_trylock");
+extern char const symbol_pthread_mutex_unlock[] asm("pthread_mutex_unlock");
+extern char const symbol_pthread_mutex_timedlock[] asm("pthread_mutex_timedlock");
+extern char const symbol_pthread_condattr_init[] asm("pthread_condattr_init");
+extern char const symbol_pthread_condattr_destroy[] asm("pthread_condattr_destroy");
+extern char const symbol_pthread_condattr_getclock[] asm("pthread_condattr_getclock");
+extern char const symbol_pthread_condattr_setclock[] asm("pthread_condattr_setclock");
+extern char const symbol_pthread_condattr_getpshared[] asm("pthread_condattr_getpshared");
+extern char const symbol_pthread_condattr_setpshared[] asm("pthread_condattr_setpshared");
+extern char const symbol_pthread_cond_init[] asm("pthread_cond_init");
+extern char const symbol_pthread_cond_destroy[] asm("pthread_cond_destroy");
+extern char const symbol_pthread_cond_signal[] asm("pthread_cond_signal");
+extern char const symbol_pthread_cond_broadcast[] asm("pthread_cond_broadcast");
+extern char const symbol_pthread_cond_wait[] asm("pthread_cond_wait");
+extern char const symbol_pthread_cond_timedwait[] asm("pthread_cond_timedwait");
+extern char const symbol_pthread_attr_init[] asm("pthread_attr_init");
+extern char const symbol_pthread_attr_destroy[] asm("pthread_attr_destroy");
+extern char const symbol_pthread_attr_getstacksize[] asm("pthread_attr_getstacksize");
+extern char const symbol_pthread_attr_setstacksize[] asm("pthread_attr_setstacksize");
+extern char const symbol_pthread_attr_getdetachstate[] asm("pthread_attr_getdetachstate");
+extern char const symbol_pthread_attr_setdetachstate[] asm("pthread_attr_setdetachstate");
+extern char const symbol_pthread_getschedparam[] asm("pthread_getschedparam");
+extern char const symbol_pthread_setschedparam[] asm("pthread_setschedparam");
+extern char const symbol_pthread_create[] asm("pthread_create");
+extern char const symbol_pthread_join[] asm("pthread_join");
+extern char const symbol_pthread_detach[] asm("pthread_detach");
+extern char const symbol_pthread_exit[] asm("pthread_exit");
+extern char const symbol_pthread_self[] asm("pthread_self");
+extern char const symbol_pthread_equal[] asm("pthread_equal");
+extern char const symbol_pthread_once[] asm("pthread_once");
+extern char const symbol_pthread_key_create[] asm("pthread_key_create");
+extern char const symbol_pthread_key_delete[] asm("pthread_key_delete");
+extern char const symbol_pthread_getspecific[] asm("pthread_getspecific");
+extern char const symbol_pthread_setspecific[] asm("pthread_setspecific");
+extern char const symbol_pthread_cancel[] asm("pthread_cancel");
+extern char const symbol_pthread_setcancelstate[] asm("pthread_setcancelstate");
+extern char const symbol_pthread_rwlock_rdlock[] asm("pthread_rwlock_rdlock");
+extern char const symbol_pthread_rwlock_tryrdlock[] asm("pthread_rwlock_tryrdlock");
+extern char const symbol_pthread_rwlock_unlock[] asm("pthread_rwlock_unlock");
+extern char const symbol_pthread_rwlock_wrlock[] asm("pthread_rwlock_wrlock");
+extern char const symbol_pthread_rwlock_trywrlock[] asm("pthread_rwlock_trywrlock");
 extern char const symbol_pax_precalc_circle_8[] asm("pax_precalc_circle_8");
 extern char const symbol_pax_precalc_uv_circle_8[] asm("pax_precalc_uv_circle_8");
 extern char const symbol_pax_precalc_circle_16[] asm("pax_precalc_circle_16");
@@ -386,6 +445,8 @@ extern char const symbol_fopencookie[] asm("fopencookie");
 extern char const symbol__fopencookie_r[] asm("_fopencookie_r");
 
 static kbelf_builtin_sym const symbols[] = {
+    { .name = "asp_device_get_name", .vaddr = (size_t) symbol_asp_device_get_name },
+    { .name = "asp_device_get_manufacturer", .vaddr = (size_t) symbol_asp_device_get_manufacturer },
     { .name = "asp_disp_get_params", .vaddr = (size_t) symbol_asp_disp_get_params },
     { .name = "asp_disp_get_fb", .vaddr = (size_t) symbol_asp_disp_get_fb },
     { .name = "asp_disp_get_pax_buf", .vaddr = (size_t) symbol_asp_disp_get_pax_buf },
@@ -396,6 +457,63 @@ static kbelf_builtin_sym const symbols[] = {
     { .name = "asp_disp_write_part_pax", .vaddr = (size_t) symbol_asp_disp_write_part_pax },
     { .name = "asp_err_id", .vaddr = (size_t) symbol_asp_err_id },
     { .name = "asp_err_desc", .vaddr = (size_t) symbol_asp_err_desc },
+    { .name = "asp_input_poll", .vaddr = (size_t) symbol_asp_input_poll },
+    { .name = "asp_input_get_nav", .vaddr = (size_t) symbol_asp_input_get_nav },
+    { .name = "asp_input_get_action", .vaddr = (size_t) symbol_asp_input_get_action },
+    { .name = "asp_input_needs_on_screen_keyboard", .vaddr = (size_t) symbol_asp_input_needs_on_screen_keyboard },
+    { .name = "asp_input_set_backlight", .vaddr = (size_t) symbol_asp_input_set_backlight },
+    { .name = "asp_input_get_backlight", .vaddr = (size_t) symbol_asp_input_get_backlight },
+    { .name = "sched_yield", .vaddr = (size_t) symbol_sched_yield },
+    { .name = "sched_get_priority_min", .vaddr = (size_t) symbol_sched_get_priority_min },
+    { .name = "sched_get_priority_max", .vaddr = (size_t) symbol_sched_get_priority_max },
+    { .name = "pthread_mutexattr_init", .vaddr = (size_t) symbol_pthread_mutexattr_init },
+    { .name = "pthread_mutexattr_destroy", .vaddr = (size_t) symbol_pthread_mutexattr_destroy },
+    { .name = "pthread_mutexattr_gettype", .vaddr = (size_t) symbol_pthread_mutexattr_gettype },
+    { .name = "pthread_mutexattr_settype", .vaddr = (size_t) symbol_pthread_mutexattr_settype },
+    { .name = "pthread_mutex_init", .vaddr = (size_t) symbol_pthread_mutex_init },
+    { .name = "pthread_mutex_destroy", .vaddr = (size_t) symbol_pthread_mutex_destroy },
+    { .name = "pthread_mutex_lock", .vaddr = (size_t) symbol_pthread_mutex_lock },
+    { .name = "pthread_mutex_trylock", .vaddr = (size_t) symbol_pthread_mutex_trylock },
+    { .name = "pthread_mutex_unlock", .vaddr = (size_t) symbol_pthread_mutex_unlock },
+    { .name = "pthread_mutex_timedlock", .vaddr = (size_t) symbol_pthread_mutex_timedlock },
+    { .name = "pthread_condattr_init", .vaddr = (size_t) symbol_pthread_condattr_init },
+    { .name = "pthread_condattr_destroy", .vaddr = (size_t) symbol_pthread_condattr_destroy },
+    { .name = "pthread_condattr_getclock", .vaddr = (size_t) symbol_pthread_condattr_getclock },
+    { .name = "pthread_condattr_setclock", .vaddr = (size_t) symbol_pthread_condattr_setclock },
+    { .name = "pthread_condattr_getpshared", .vaddr = (size_t) symbol_pthread_condattr_getpshared },
+    { .name = "pthread_condattr_setpshared", .vaddr = (size_t) symbol_pthread_condattr_setpshared },
+    { .name = "pthread_cond_init", .vaddr = (size_t) symbol_pthread_cond_init },
+    { .name = "pthread_cond_destroy", .vaddr = (size_t) symbol_pthread_cond_destroy },
+    { .name = "pthread_cond_signal", .vaddr = (size_t) symbol_pthread_cond_signal },
+    { .name = "pthread_cond_broadcast", .vaddr = (size_t) symbol_pthread_cond_broadcast },
+    { .name = "pthread_cond_wait", .vaddr = (size_t) symbol_pthread_cond_wait },
+    { .name = "pthread_cond_timedwait", .vaddr = (size_t) symbol_pthread_cond_timedwait },
+    { .name = "pthread_attr_init", .vaddr = (size_t) symbol_pthread_attr_init },
+    { .name = "pthread_attr_destroy", .vaddr = (size_t) symbol_pthread_attr_destroy },
+    { .name = "pthread_attr_getstacksize", .vaddr = (size_t) symbol_pthread_attr_getstacksize },
+    { .name = "pthread_attr_setstacksize", .vaddr = (size_t) symbol_pthread_attr_setstacksize },
+    { .name = "pthread_attr_getdetachstate", .vaddr = (size_t) symbol_pthread_attr_getdetachstate },
+    { .name = "pthread_attr_setdetachstate", .vaddr = (size_t) symbol_pthread_attr_setdetachstate },
+    { .name = "pthread_getschedparam", .vaddr = (size_t) symbol_pthread_getschedparam },
+    { .name = "pthread_setschedparam", .vaddr = (size_t) symbol_pthread_setschedparam },
+    { .name = "pthread_create", .vaddr = (size_t) symbol_pthread_create },
+    { .name = "pthread_join", .vaddr = (size_t) symbol_pthread_join },
+    { .name = "pthread_detach", .vaddr = (size_t) symbol_pthread_detach },
+    { .name = "pthread_exit", .vaddr = (size_t) symbol_pthread_exit },
+    { .name = "pthread_self", .vaddr = (size_t) symbol_pthread_self },
+    { .name = "pthread_equal", .vaddr = (size_t) symbol_pthread_equal },
+    { .name = "pthread_once", .vaddr = (size_t) symbol_pthread_once },
+    { .name = "pthread_key_create", .vaddr = (size_t) symbol_pthread_key_create },
+    { .name = "pthread_key_delete", .vaddr = (size_t) symbol_pthread_key_delete },
+    { .name = "pthread_getspecific", .vaddr = (size_t) symbol_pthread_getspecific },
+    { .name = "pthread_setspecific", .vaddr = (size_t) symbol_pthread_setspecific },
+    { .name = "pthread_cancel", .vaddr = (size_t) symbol_pthread_cancel },
+    { .name = "pthread_setcancelstate", .vaddr = (size_t) symbol_pthread_setcancelstate },
+    { .name = "pthread_rwlock_rdlock", .vaddr = (size_t) symbol_pthread_rwlock_rdlock },
+    { .name = "pthread_rwlock_tryrdlock", .vaddr = (size_t) symbol_pthread_rwlock_tryrdlock },
+    { .name = "pthread_rwlock_unlock", .vaddr = (size_t) symbol_pthread_rwlock_unlock },
+    { .name = "pthread_rwlock_wrlock", .vaddr = (size_t) symbol_pthread_rwlock_wrlock },
+    { .name = "pthread_rwlock_trywrlock", .vaddr = (size_t) symbol_pthread_rwlock_trywrlock },
     { .name = "pax_precalc_circle_8", .vaddr = (size_t) symbol_pax_precalc_circle_8 },
     { .name = "pax_precalc_uv_circle_8", .vaddr = (size_t) symbol_pax_precalc_uv_circle_8 },
     { .name = "pax_precalc_circle_16", .vaddr = (size_t) symbol_pax_precalc_circle_16 },
@@ -771,6 +889,6 @@ static kbelf_builtin_sym const symbols[] = {
 
 kbelf_builtin_lib const badge_elf_lib = {
     .path        = "libbadge.so",
-    .symbols_len = 381,
+    .symbols_len = 440,
     .symbols     = symbols,
 };
