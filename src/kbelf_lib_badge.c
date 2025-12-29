1b7448a726781468f94f69273562c36f659e5323
Rene Schickbauer <github@cavac.at>
2025-12-29 23:41:02 +0100

    Add stuff for plugin support

--- src/kbelf_lib_badge.c
+++ src/kbelf_lib_badge.c
@@ -28,6 +28,59 @@ extern char const symbol_asp_input_get_action[] asm("asp_input_get_action");
 extern char const symbol_asp_input_needs_on_screen_keyboard[] asm("asp_input_needs_on_screen_keyboard");
 extern char const symbol_asp_input_set_backlight[] asm("asp_input_set_backlight");
 extern char const symbol_asp_input_get_backlight[] asm("asp_input_get_backlight");
+extern char const symbol_asp_log_info[] asm("asp_log_info");
+extern char const symbol_asp_log_warn[] asm("asp_log_warn");
+extern char const symbol_asp_log_error[] asm("asp_log_error");
+extern char const symbol_asp_disp_flush[] asm("asp_disp_flush");
+extern char const symbol_asp_disp_flush_region[] asm("asp_disp_flush_region");
+extern char const symbol_asp_plugin_status_widget_register[] asm("asp_plugin_status_widget_register");
+extern char const symbol_asp_plugin_status_widget_unregister[] asm("asp_plugin_status_widget_unregister");
+extern char const symbol_asp_plugin_input_poll[] asm("asp_plugin_input_poll");
+extern char const symbol_asp_plugin_input_get_key_state[] asm("asp_plugin_input_get_key_state");
+extern char const symbol_asp_plugin_input_hook_register[] asm("asp_plugin_input_hook_register");
+extern char const symbol_asp_plugin_input_hook_unregister[] asm("asp_plugin_input_hook_unregister");
+extern char const symbol_asp_plugin_input_inject[] asm("asp_plugin_input_inject");
+extern char const symbol_asp_led_set_brightness[] asm("asp_led_set_brightness");
+extern char const symbol_asp_led_get_brightness[] asm("asp_led_get_brightness");
+extern char const symbol_asp_led_set_mode[] asm("asp_led_set_mode");
+extern char const symbol_asp_led_get_mode[] asm("asp_led_get_mode");
+extern char const symbol_asp_led_set_pixel[] asm("asp_led_set_pixel");
+extern char const symbol_asp_led_set_pixel_rgb[] asm("asp_led_set_pixel_rgb");
+extern char const symbol_asp_led_set_pixel_hsv[] asm("asp_led_set_pixel_hsv");
+extern char const symbol_asp_led_send[] asm("asp_led_send");
+extern char const symbol_asp_led_clear[] asm("asp_led_clear");
+extern char const symbol_asp_plugin_storage_open[] asm("asp_plugin_storage_open");
+extern char const symbol_asp_plugin_storage_read[] asm("asp_plugin_storage_read");
+extern char const symbol_asp_plugin_storage_write[] asm("asp_plugin_storage_write");
+extern char const symbol_asp_plugin_storage_seek[] asm("asp_plugin_storage_seek");
+extern char const symbol_asp_plugin_storage_tell[] asm("asp_plugin_storage_tell");
+extern char const symbol_asp_plugin_storage_close[] asm("asp_plugin_storage_close");
+extern char const symbol_asp_plugin_storage_exists[] asm("asp_plugin_storage_exists");
+extern char const symbol_asp_plugin_storage_mkdir[] asm("asp_plugin_storage_mkdir");
+extern char const symbol_asp_plugin_storage_remove[] asm("asp_plugin_storage_remove");
+extern char const symbol_asp_plugin_delay_ms[] asm("asp_plugin_delay_ms");
+extern char const symbol_asp_plugin_get_tick_ms[] asm("asp_plugin_get_tick_ms");
+extern char const symbol_asp_plugin_should_stop[] asm("asp_plugin_should_stop");
+extern char const symbol_asp_plugin_menu_add_item[] asm("asp_plugin_menu_add_item");
+extern char const symbol_asp_plugin_menu_remove_item[] asm("asp_plugin_menu_remove_item");
+extern char const symbol_asp_plugin_event_register[] asm("asp_plugin_event_register");
+extern char const symbol_asp_plugin_event_unregister[] asm("asp_plugin_event_unregister");
+extern char const symbol_asp_net_is_connected[] asm("asp_net_is_connected");
+extern char const symbol_asp_http_get[] asm("asp_http_get");
+extern char const symbol_asp_http_post[] asm("asp_http_post");
+extern char const symbol_asp_plugin_settings_get_string[] asm("asp_plugin_settings_get_string");
+extern char const symbol_asp_plugin_settings_set_string[] asm("asp_plugin_settings_set_string");
+extern char const symbol_asp_plugin_settings_get_int[] asm("asp_plugin_settings_get_int");
+extern char const symbol_asp_plugin_settings_set_int[] asm("asp_plugin_settings_set_int");
+extern char const symbol_asp_power_get_battery_info[] asm("asp_power_get_battery_info");
+extern char const symbol_asp_power_get_system_voltage[] asm("asp_power_get_system_voltage");
+extern char const symbol_asp_power_get_battery_voltage[] asm("asp_power_get_battery_voltage");
+extern char const symbol_asp_power_get_input_voltage[] asm("asp_power_get_input_voltage");
+extern char const symbol_asp_power_get_charging_config[] asm("asp_power_get_charging_config");
+extern char const symbol_asp_power_set_charging[] asm("asp_power_set_charging");
+extern char const symbol_asp_power_get_usb_boost[] asm("asp_power_get_usb_boost");
+extern char const symbol_asp_power_set_usb_boost[] asm("asp_power_set_usb_boost");
+extern char const symbol_chakrapetchmedium[] asm("chakrapetchmedium");
 
 static kbelf_builtin_sym const symbols[] = {
     { .name = "asp_audio_set_rate", .vaddr = (size_t) symbol_asp_audio_set_rate },
@@ -55,10 +108,63 @@ static kbelf_builtin_sym const symbols[] = {
     { .name = "asp_input_needs_on_screen_keyboard", .vaddr = (size_t) symbol_asp_input_needs_on_screen_keyboard },
     { .name = "asp_input_set_backlight", .vaddr = (size_t) symbol_asp_input_set_backlight },
     { .name = "asp_input_get_backlight", .vaddr = (size_t) symbol_asp_input_get_backlight },
+    { .name = "asp_log_info", .vaddr = (size_t) symbol_asp_log_info },
+    { .name = "asp_log_warn", .vaddr = (size_t) symbol_asp_log_warn },
+    { .name = "asp_log_error", .vaddr = (size_t) symbol_asp_log_error },
+    { .name = "asp_disp_flush", .vaddr = (size_t) symbol_asp_disp_flush },
+    { .name = "asp_disp_flush_region", .vaddr = (size_t) symbol_asp_disp_flush_region },
+    { .name = "asp_plugin_status_widget_register", .vaddr = (size_t) symbol_asp_plugin_status_widget_register },
+    { .name = "asp_plugin_status_widget_unregister", .vaddr = (size_t) symbol_asp_plugin_status_widget_unregister },
+    { .name = "asp_plugin_input_poll", .vaddr = (size_t) symbol_asp_plugin_input_poll },
+    { .name = "asp_plugin_input_get_key_state", .vaddr = (size_t) symbol_asp_plugin_input_get_key_state },
+    { .name = "asp_plugin_input_hook_register", .vaddr = (size_t) symbol_asp_plugin_input_hook_register },
+    { .name = "asp_plugin_input_hook_unregister", .vaddr = (size_t) symbol_asp_plugin_input_hook_unregister },
+    { .name = "asp_plugin_input_inject", .vaddr = (size_t) symbol_asp_plugin_input_inject },
+    { .name = "asp_led_set_brightness", .vaddr = (size_t) symbol_asp_led_set_brightness },
+    { .name = "asp_led_get_brightness", .vaddr = (size_t) symbol_asp_led_get_brightness },
+    { .name = "asp_led_set_mode", .vaddr = (size_t) symbol_asp_led_set_mode },
+    { .name = "asp_led_get_mode", .vaddr = (size_t) symbol_asp_led_get_mode },
+    { .name = "asp_led_set_pixel", .vaddr = (size_t) symbol_asp_led_set_pixel },
+    { .name = "asp_led_set_pixel_rgb", .vaddr = (size_t) symbol_asp_led_set_pixel_rgb },
+    { .name = "asp_led_set_pixel_hsv", .vaddr = (size_t) symbol_asp_led_set_pixel_hsv },
+    { .name = "asp_led_send", .vaddr = (size_t) symbol_asp_led_send },
+    { .name = "asp_led_clear", .vaddr = (size_t) symbol_asp_led_clear },
+    { .name = "asp_plugin_storage_open", .vaddr = (size_t) symbol_asp_plugin_storage_open },
+    { .name = "asp_plugin_storage_read", .vaddr = (size_t) symbol_asp_plugin_storage_read },
+    { .name = "asp_plugin_storage_write", .vaddr = (size_t) symbol_asp_plugin_storage_write },
+    { .name = "asp_plugin_storage_seek", .vaddr = (size_t) symbol_asp_plugin_storage_seek },
+    { .name = "asp_plugin_storage_tell", .vaddr = (size_t) symbol_asp_plugin_storage_tell },
+    { .name = "asp_plugin_storage_close", .vaddr = (size_t) symbol_asp_plugin_storage_close },
+    { .name = "asp_plugin_storage_exists", .vaddr = (size_t) symbol_asp_plugin_storage_exists },
+    { .name = "asp_plugin_storage_mkdir", .vaddr = (size_t) symbol_asp_plugin_storage_mkdir },
+    { .name = "asp_plugin_storage_remove", .vaddr = (size_t) symbol_asp_plugin_storage_remove },
+    { .name = "asp_plugin_delay_ms", .vaddr = (size_t) symbol_asp_plugin_delay_ms },
+    { .name = "asp_plugin_get_tick_ms", .vaddr = (size_t) symbol_asp_plugin_get_tick_ms },
+    { .name = "asp_plugin_should_stop", .vaddr = (size_t) symbol_asp_plugin_should_stop },
+    { .name = "asp_plugin_menu_add_item", .vaddr = (size_t) symbol_asp_plugin_menu_add_item },
+    { .name = "asp_plugin_menu_remove_item", .vaddr = (size_t) symbol_asp_plugin_menu_remove_item },
+    { .name = "asp_plugin_event_register", .vaddr = (size_t) symbol_asp_plugin_event_register },
+    { .name = "asp_plugin_event_unregister", .vaddr = (size_t) symbol_asp_plugin_event_unregister },
+    { .name = "asp_net_is_connected", .vaddr = (size_t) symbol_asp_net_is_connected },
+    { .name = "asp_http_get", .vaddr = (size_t) symbol_asp_http_get },
+    { .name = "asp_http_post", .vaddr = (size_t) symbol_asp_http_post },
+    { .name = "asp_plugin_settings_get_string", .vaddr = (size_t) symbol_asp_plugin_settings_get_string },
+    { .name = "asp_plugin_settings_set_string", .vaddr = (size_t) symbol_asp_plugin_settings_set_string },
+    { .name = "asp_plugin_settings_get_int", .vaddr = (size_t) symbol_asp_plugin_settings_get_int },
+    { .name = "asp_plugin_settings_set_int", .vaddr = (size_t) symbol_asp_plugin_settings_set_int },
+    { .name = "asp_power_get_battery_info", .vaddr = (size_t) symbol_asp_power_get_battery_info },
+    { .name = "asp_power_get_system_voltage", .vaddr = (size_t) symbol_asp_power_get_system_voltage },
+    { .name = "asp_power_get_battery_voltage", .vaddr = (size_t) symbol_asp_power_get_battery_voltage },
+    { .name = "asp_power_get_input_voltage", .vaddr = (size_t) symbol_asp_power_get_input_voltage },
+    { .name = "asp_power_get_charging_config", .vaddr = (size_t) symbol_asp_power_get_charging_config },
+    { .name = "asp_power_set_charging", .vaddr = (size_t) symbol_asp_power_set_charging },
+    { .name = "asp_power_get_usb_boost", .vaddr = (size_t) symbol_asp_power_get_usb_boost },
+    { .name = "asp_power_set_usb_boost", .vaddr = (size_t) symbol_asp_power_set_usb_boost },
+    { .name = "chakrapetchmedium", .vaddr = (size_t) symbol_chakrapetchmedium },
 };
 
 kbelf_builtin_lib const badge_elf_lib_badge = {
     .path        = "libbadge.so",
-    .symbols_len = 25,
+    .symbols_len = 78,
     .symbols     = symbols,
 };
